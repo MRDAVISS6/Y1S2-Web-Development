c1fbcfa5fdfcd3a56984e9673029aaabfb72b8c9
Samuel Davies (1000048175) <DAVISS6@student.op.ac.nz>
2025-09-17 13:38:50 +1200

    Added Initial Svelte APP

---
+++ Web1SvelteIntro/src/routes/about/+page.svelte
@@ -0,0 +1,12 @@
+<h1>{title}</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    let title = $state("About");
+</script>
+<p>Welcome to my About page</p>
+<nav>
+    <a href = "/">Home</a>
+    <a href = "/about">About</a>
+    <a href = "/content">Content</a>
+    <a href = "/">ET PHONE HOME</a>
+</nav>
